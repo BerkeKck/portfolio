6da90ab22d94a21f93e1e8cfd278dff7ead09113
Berke Küçük <brke.kucuk.22@gmail.com>
2024-03-03 16:15:24 +0300

    Update dark-mode.js

--- src/utils/dark-mode.js
+++ src/utils/dark-mode.js
@@ -1,11 +1,27 @@
 const darkMode = () => {
 const themeToggleButtons = document.querySelectorAll('#theme-toggle');
 
-themeToggleButtons.forEach(btn => {
-    btn.addEventListener('click', () => {
-        document.body.classList.toggle('light-mode');
-    });
-});
+//state
+const theme = localStorage.getItem('theme');
+
+//on mount
+theme && document.body.classList.add(theme);   //short circuting
+
+//handlers
+const handleThemeToggle = () => {
+    document.body.classList.toggle('light-mode');
+        if (document.body.classList.contains('light-mode')) {
+        localStorage.setItem('theme','light-mode');
+        } else {
+        localStorage.removeItem('theme');
+        document.body.removeAttribute('class');
+        }
+}
+
+//events
+themeToggleButtons.forEach(btn => 
+    btn.addEventListener('click', handleThemeToggle)
+    );
 };
 
 export default darkMode;
