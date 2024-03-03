bbbc77f0d64c060eaa90545c623887680ae7ca73
Berke Küçük <brke.kucuk.22@gmail.com>
2024-03-03 15:18:23 +0300

    dark/light mode

---
+++ src/utils/dark-mode.js
@@ -0,0 +1,11 @@
+const darkMode = () => {
+const themeToggleButtons = document.querySelectorAll('#theme-toggle');
+
+themeToggleButtons.forEach(btn => {
+    btn.addEventListener('click', () => {
+        document.body.classList.toggle('light-mode');
+    });
+});
+};
+
+export default darkMode;
--- vite.config.js
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from "vite";
-import typeScript from '@vitejs/plugin-typeScript'
-
-export default defineConfig({
-    plugins: [react()],
-})
